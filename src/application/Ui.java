ae69303bb881463837f27c83dc3ba70bf937a7a4
Aleykson Micael Sampaio De Andrade <aleyksonmicael@gmail.com>
2022-07-09 12:19:15 -0300

    criando verificação das posições

--- src/application/Ui.java
+++ src/application/Ui.java
@@ -5,17 +5,18 @@ import chess.ChessPiece;
 public class Ui {
 
     public static void printBoard(ChessPiece[][] pieces){
-        System.out.println("  ┌—————————————————┐");
+        System.out.println("               XADREZ           ");
+        System.out.println("     ┌—————————————————————————┐");
         for (int i=0; i<pieces.length; i++){
-            System.out.print((8 - i) + " | ");
+            System.out.print("   "+(8 - i) + " | ");
             for (int j=0; j<pieces.length; j++){
                 printPiece(pieces[i][j]);
             }
             System.out.println("|");
         }
 
-        System.out.println("  └—————————————————┘");
-        System.out.println("    A B C D E F G H");
+        System.out.println("     └—————————————————————————┘");
+        System.out.println("       A  B  C  D  E  F  G  H");
 
     }
     private static void printPiece(ChessPiece piece){
@@ -24,6 +25,6 @@ public class Ui {
         }else {
             System.out.print(piece);
         }
-        System.out.print(" ");
+        System.out.print("  ");
     }
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(Integer rows, Integer columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Erro de tabuleiro: Deve-se ter pelo menos 1 linha e 1 coluna");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -16,21 +19,46 @@ public class Board {
         return rows;
     }
 
-    public void setRows(Integer rows) {
-        this.rows = rows;
-    }
-
     public Integer getColumns() {
         return columns;
     }
 
-    public void setColumns(Integer columns) {
-        this.columns = columns;
+    public Piece piece(Integer row, Integer column) {
+        if (!positionExists(row,column)){
+            throw new BoardException("Não existe esta posição no tabuleiro!");
+        }
+        return pieces[row][column];
     }
-    public Piece piece (Integer row, Integer columns){
-        return pieces[row][columns];
-    }
-    public Piece piece (Position position){
+
+    public Piece piece(Position position) {
+        // PODE ESTA ERRADO ESSA VERIFICAÇÃO!
+        if (!posirionExists(position)){
+            throw new BoardException("Não existe esta posição no tabuleiro!");
+        }
         return pieces[position.getRow()][position.getColum()];
     }
+
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)){
+            throw new BoardException("Já existe uma peça na posição !" + position + " !");
+        }
+        this.pieces[position.getRow()][position.getColum()] = piece;
+        piece.position = position;
+    }
+
+    public boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean posirionExists(Position position) {
+        return positionExists(position.getRow(), position.getColum());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!posirionExists(position)){
+            throw new BoardException("Não existe esta posição no tabuleiro!");
+        }
+        return piece(position) != null;
+    }
+
 }
